043101ff6a686430377e1e18d48b7e4db79bc8c7
SarkiIhima <sarkiihima44@gmail.com>
2025-02-05 22:10:02 +0100

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,49 +1,49 @@
--- create table buckets (
---     id serial primary key,
---     name varchar(255) not null unique,
---     url varchar(255),
---     created_at timestamp not null,
---     updated_at timestamp
--- );
+create table buckets (
+    id serial primary key,
+    name varchar(255) not null unique,
+    url varchar(255),
+    created_at timestamp not null,
+    updated_at timestamp
+);
 
 
--- create table users (
---     id serial primary key,
---     name varchar(255),
---     username varchar(255),
---     email varchar(255) unique not null,
---     password varchar(255) not null,
---     created_at timestamp not null,
---     updated_at timestamp
--- );
+create table users (
+    id serial primary key,
+    name varchar(255),
+    username varchar(255),
+    email varchar(255) unique not null,
+    password varchar(255) not null,
+    created_at timestamp not null,
+    updated_at timestamp
+);
 
--- create table drives (
---     id serial primary key,
---     name varchar(255),
---     is_personal boolean,
---     owner_id int references users(id),
---     bucket_id int references buckets(id),
---     created_at timestamp not null,
---     updated_at timestamp
--- );
+create table drives (
+    id serial primary key,
+    name varchar(255),
+    is_personal boolean,
+    owner_id int references users(id),
+    bucket_id int references buckets(id),
+    created_at timestamp not null,
+    updated_at timestamp
+);
 
--- create table drive_members (
---     id serial primary key,
---     user_id int references users(id) not null,
---     drive_id int references drives(id) not null,
---     created_at timestamp not null,
---     updated_at timestamp
--- );
+create table drive_members (
+    id serial primary key,
+    user_id int references users(id) not null,
+    drive_id int references drives(id) not null,
+    created_at timestamp not null,
+    updated_at timestamp
+);
 
--- create table objects (
---     id serial primary key,
---     name varchar(255),
---     is_dir boolean,
---     drive_id int references drives(id),
---     metadata json,
---     created_at timestamp not null,
---     updated_at timestamp
--- );
+create table objects (
+    id serial primary key,
+    name varchar(255),
+    is_dir boolean,
+    drive_id int references drives(id),
+    metadata json,
+    created_at timestamp not null,
+    updated_at timestamp
+);
 
 create table shares (
     id serial primary key,
